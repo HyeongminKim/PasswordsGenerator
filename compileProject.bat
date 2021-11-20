e4a8aa3158ec8212f26aca1882cf2e80f4e1e62d
HyeongminKim <assa0620@gmail.com>
2021-11-20 23:43:14 +0900

    스크립트 예외처리 및 자동 종료 텍스트 표기

--- compileProject.bat
+++ compileProject.bat
@@ -4,17 +4,44 @@ echo LICENSE: This script is licensed under the MIT License
 
 echo NOTE: This window closes automatically.
 echo.
-chcp 65001
 
+if not exist PasswordsGenerator (
+    echo FATAL ERROR! This script obviously shouldn't be outside the project.
+    echo This script was aborted, but it didn't affect the current working directory.
+    echo This window will close automatically after 15 seconds...
+    timeout 15 > NUL
+    exit
+)
+
+chcp 65001
 cd PasswordsGenerator
 echo Compiling passwdGen.java file...
 
 javac -encoding utf-8 passwdGen.java
+if "%ERRORLEVEL%" == "1" (
+    echo.
+    echo The build seems to have failed. Please check again for syntax errors in the recently edited line.
+    echo If you want to see StackTrace for a long time, try compiling it manually.
+    echo This window will close automatically after 60 seconds...
+    timeout 60 > NUL
+    exit
+)
 
 echo Generating passwdGen.jar file...
 jar -cmf Manifest.mf passwdGen.jar *.class
+if "%ERRORLEVEL%" == "1" (
+    echo.
+    echo jar file couldn't be created. Please check if you are targeting the wrong class file.
+    echo If you want to see log for a long time, try it manually.
+    echo This window will close automatically after 60 seconds...
+    timeout 60 > NUL
+    exit
+)
 
 echo Cleanup cache files...
 del *.class
+
+echo This window will close automatically after 5 seconds...
+timeout 5 > NUL
 
 exit
