c81f79c8251c8ca85e305b448268c9a4c7938d21
HyeongminKim <assa0620@gmail.com>
2021-11-21 17:44:50 +0900

    실행 가능한 OS지정 및 exit 코드 재지정

--- compileProject.sh
+++ compileProject.sh
@@ -8,25 +8,31 @@ if [ $? != 0 ]; then
     exit 1
 fi
 
+
+if [ "$(uname -s)" != "Darwin" ]; then
+    echo -e "\033[31mERROR! This script only support macOS"
+    exit 3
+fi
+
 cd PasswordsGenerator 2>/dev/null
 if [ $? != 0 ]; then
     echo -e "\033[31mFATAL ERROR! This script obviously shouldn't be outside the project."
     echo -e "This script was aborted, but it didn't affect the current working directory. \033[m"
-    exit 1
+    exit 2
 fi
 
 echo "Compiling passwdGen.java file..."
 javac passwdGen.java
 if [ $? != 0 ]; then
     echo -e "\nThe build seems to have failed. Please check again for syntax errors in the recently edited line."
-    exit 1
+    exit 2
 fi
 
 echo "Generating passwdGen.jar file..."
 jar -cmf Manifest.mf passwdGen.jar *.class
 if [ $? != 0 ]; then
     echo "jar file couldn't be created. Please check if you are targeting the wrong class file."
-    exit 1
+    exit 2
 fi
 
 echo "Cleanup cache files..."
