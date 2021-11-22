32a2729f4bbee79eae795011c93bffe0bdad2915
HyeongminKim <assa0620@gmail.com>
2021-11-22 14:30:11 +0900

    코드 최적화

--- PasswordsGenerator/passwdGen.java
+++ PasswordsGenerator/passwdGen.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.datatransfer.*;
 
-public class passwdGen implements WindowListener {
+public class passwdGen extends WindowAdapter {
     private Frame frame;
     private Panel header, passCnt, context, action, footer;
     private Label welcomeText, pwdCnt, placeholderText;
@@ -242,25 +242,7 @@ public class passwdGen implements WindowListener {
     }
 
     @Override
-    public void windowActivated(WindowEvent arg0) { }
-
-    @Override
-    public void windowClosed(WindowEvent arg0) { }
-
-    @Override
     public void windowClosing(WindowEvent arg0) {
         System.exit(0);
     }
-
-    @Override
-    public void windowDeactivated(WindowEvent arg0) { }
-
-    @Override
-    public void windowDeiconified(WindowEvent arg0) { }
-
-    @Override
-     public void windowIconified(WindowEvent arg0) { }
-
-    @Override
-    public void windowOpened(WindowEvent arg0) { }
 }
