c412bf51c049ab0b354bbb0c3955f46e422e84ec
HyeongminKim <assa0620@gmail.com>
2021-11-28 23:47:24 +0900

    패스워드 다수 생성기능 추가 및 생성 결과 UI 제작

--- PasswordsGenerator/PasswdGen.java
+++ PasswordsGenerator/PasswdGen.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import javax.swing.*;
 
 public class PasswdGen extends WindowAdapter {
-    private Frame frame;
+    private Frame mainView, resultView;
     private Panel header, passCnt, context, action, footer;
     private Label welcomeText, pwdCnt, placeholderText, cmdPrompt;
     private TextField passwordCount;
@@ -18,6 +18,7 @@ public class PasswdGen extends WindowAdapter {
     private Button generate, clear, openWeb;
     private TextArea resultOutput;
     private boolean generated, exceptSimilarSymbol, generalSymbol;
+    private int createPassword = 1;
     private String similarSymbol = "1l|Ii!joO0;:9gqxX.,", exceptUniqueSymbol = "";
 
     public PasswdGen() {
@@ -42,7 +43,7 @@ public class PasswdGen extends WindowAdapter {
 
         if(length < 8 || !(caps && num && unique)) {
             formatedLogcat("WRN", "비밀번호 정책이 규정에 부합하지 않음");
-            showAlert(target, frame.getTitle() + "- 경고", "현재 설정된 비밀번호 정책은 대부분의 사이트에서 허용하지 않으며 보안에 치명적이므로 이 비밀번호를 사용하는 것은 권장하지 않습니다. ");
+            showAlert(target, mainView.getTitle() + "- 경고", "현재 설정된 비밀번호 정책은 대부분의 사이트에서 허용하지 않으며 보안에 치명적이므로 이 비밀번호를 사용하는 것은 권장하지 않습니다. ");
         }
 
         int resultCount = 0;
@@ -167,7 +168,7 @@ public class PasswdGen extends WindowAdapter {
                     break;
             }
         }
-        
+
         return result;
     }
 
@@ -186,7 +187,7 @@ public class PasswdGen extends WindowAdapter {
                 alert.dispose();
             }
         });
-        
+
         alert.add("North", msg);
         alert.add("South", submit);
         alert.pack();
@@ -212,7 +213,7 @@ public class PasswdGen extends WindowAdapter {
     }
 
     private void init() {
-        frame = new Frame("비밀번호 생성기");
+        mainView = new Frame("비밀번호 생성기");
         header = new Panel();
         passCnt = new Panel();
         context = new Panel();
@@ -237,7 +238,7 @@ public class PasswdGen extends WindowAdapter {
         openWeb = new Button("GitHub");
 
         resultOutput = new TextArea();
-        formatedLogcat("INFO", "passwdGen 초기화... [" + frame.getTitle() + "] 윈도우 생성됨", false);
+        formatedLogcat("INFO", "passwdGen 초기화... [" + mainView.getTitle() + "] 윈도우 생성됨", false);
         resultOutput.setEditable(false);
 
         header.add(welcomeText);
@@ -262,45 +263,99 @@ public class PasswdGen extends WindowAdapter {
         commandLine.add(openWeb);
         footer.add("South", commandLine);
 
-        frame.setSize(600, 150);
-        frame.setLayout(new BorderLayout(1, 2));
-        frame.setResizable(false);
+        mainView.setSize(600, 150);
+        mainView.setLayout(new BorderLayout(1, 2));
+        mainView.setResizable(false);
 
-        frame.add("North", header);
-        frame.add("West", passCnt);
-        frame.add("Center", context);
-        frame.add("East", action);
-        frame.add("South", footer);
+        mainView.add("North", header);
+        mainView.add("West", passCnt);
+        mainView.add("Center", context);
+        mainView.add("East", action);
+        mainView.add("South", footer);
 
-        frame.pack();
-        frame.setVisible(true);
-        frame.addWindowListener(this);
+        mainView.pack();
+        mainView.setVisible(true);
+        mainView.addWindowListener(this);
     }
 
     private void setAction() {
         generate.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
+                    String[] result = new String[20];
+
                     passwordCount.setText(passwordCount.getText().trim());
                     if (passwordCount.getText().equals("")) {
                         throw new NumberFormatException("No length value entered.");
                     }
-                    String result = generatePassword(frame, Integer.parseInt(passwordCount.getText()), containCaps.getState(), containNum.getState(), containUniqueSymbol.getState());
-                    if (result == null || result.length() != Integer.parseInt(passwordCount.getText())) {
-                        throw new NullPointerException();
+                    for(int i = 0; i < createPassword; i++) {
+                        result[i] = generatePassword(mainView, Integer.parseInt(passwordCount.getText()), containCaps.getState(), containNum.getState(), containUniqueSymbol.getState());
+                        if (result[i] == null || result[i].length() != Integer.parseInt(passwordCount.getText())) {
+                            throw new NullPointerException();
+                        }
                     }
-                    StringSelection selection = new StringSelection(result);
-                    Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-                    clipboard.setContents(selection, selection);
-                    generated = true;
                     formatedLogcat("INFO", "비밀번호가 성공적으로 생성되었습니다. ");
-                    showAlert(frame, frame.getTitle(), "생성된 비밀번호는: " + result + " 입니다. 이 비밀번호는 클립보드에 이미 저장되었으므로 스크린 샷을 따로 촬영하지 않아도 됩니다. ");
+
+                    resultView = new Frame("비밀번호 생성 결과");
+                    Choice resultContainer = new Choice();
+                    resultContainer.addItemListener(new ItemListener() {
+                        public void itemStateChanged(ItemEvent e) {
+                            resultContainer.repaint();
+                        }
+
+                        public void paint(Graphics g) {
+                            resultContainer.paint(g);
+                            g.setColor(Color.BLUE);
+                            String msg = "비밀번호 선택";
+                            msg += resultContainer.getSelectedItem();
+                            g.drawString(msg, 30, 120);
+                        }
+                    });
+
+                    for(int i = 0; i < createPassword; i++) {
+                        resultContainer.add(result[i]);
+                    }
+
+                    Button copyPassword = new Button("복사 후 닫기");
+                    copyPassword.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent evt) {
+                            StringSelection selection = new StringSelection(result[resultContainer.getSelectedIndex()]);
+                            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                            clipboard.setContents(selection, selection);
+                            generated = true;
+                            formatedLogcat("INFO", "비밀번호 생성 결과창이 비활성화 됨");
+                            resultView.dispose();
+                        }
+                    });
+                    Button dismiss = new Button("닫기");
+                    dismiss.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent evt) {
+                            formatedLogcat("INFO", "비밀번호 생성 결과창이 비활성화 됨");
+                            resultView.dispose();
+                        }
+                    });
+
+                    Panel up = new Panel();
+                    Panel down = new Panel();
+                    resultView.setLayout(new BorderLayout(1, 2));
+                    down.setLayout(new BorderLayout(1, 2));
+                    up.add(resultContainer);
+                    down.add("West", copyPassword);
+                    down.add("East", dismiss);
+                    resultView.add("North", up);
+                    resultView.add("South", down);
+                    resultView.setSize(300, 200);
+                    resultView.pack();
+                    resultView.setLocation(50, 50);
+                    formatedLogcat("INFO", "비밀번호 생성 결과창이 활성화 됨");
+                    resultView.setVisible(true);
+
                 } catch(NumberFormatException exception) {
                     formatedLogcat("ERR", "비밀번호를 생성하는 도중 예외 발생: " + exception.toString() + "\n\t" + passwordCount.getText() + " (은)는 비밀번호 길이를 생성할 수 있는 올바른 숫자가 아닙니다. ");
-                    showAlert(frame, frame.getTitle() + "- 오류", "비밀번호 길이 필드에는 자연수만 입력해야 합니다. 비밀번호 길이 필드를 다시 한번 확인하시길 바랍니다. ");
+                    showAlert(mainView, mainView.getTitle() + "- 오류", "비밀번호 길이 필드에는 자연수만 입력해야 합니다. 비밀번호 길이 필드를 다시 한번 확인하시길 바랍니다. ");
                 } catch(NullPointerException exception) {
                     formatedLogcat("ERR", "비밀번호를 생성하는 도중 예외 발생: " + exception.toString() + "\n\t" + "이 예외를 https://github.com/HyeongminKim (@HyeongminKim) 에게 제보하세요. ");
-                    showAlert(frame, frame.getTitle() + "- 오류", "알 수 없는 이유로 비밀번호가 생성되지 않았습니다. 주로 잘못된 규칙을 설정했거나 프로그램 버그로 인해 이 문제가 발생합니다. 설정하신 규칙과 이 메시지를 함께 스크린 샷을 촬영하여 이슈를 제보하여 주세요. ");
+                    showAlert(mainView, mainView.getTitle() + "- 오류", "알 수 없는 이유로 비밀번호가 생성되지 않았습니다. 주로 잘못된 규칙을 설정했거나 프로그램 버그로 인해 이 문제가 발생합니다. 설정하신 규칙과 이 메시지를 함께 스크린 샷을 촬영하여 이슈를 제보하여 주세요. ");
                 }
             }
         });
@@ -309,7 +364,7 @@ public class PasswdGen extends WindowAdapter {
             public void actionPerformed(ActionEvent e) {
                 if(!generated) {
                     formatedLogcat("ERR", "클립보드를 지울 수 없습니다. 먼저 비밀번호를 생성하시길 바랍니다. ");
-                    showAlert(frame, frame.getTitle() + "- 오류", "비밀번호를 한 번도 생성하지 않았거나 이미 클립보드가 비워진 상태입니다. ");;
+                    showAlert(mainView, mainView.getTitle() + "- 오류", "비밀번호를 한 번도 복사하지 않았거나 이미 클립보드가 비워진 상태입니다. ");;
                 } else {
                     StringSelection selection = new StringSelection("");
                     Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
@@ -317,7 +372,7 @@ public class PasswdGen extends WindowAdapter {
                     generated = false;
                     resultOutput.setText(null);
                     formatedLogcat("INFO", "클립보드 지우기 완료", false);
-                    showAlert(frame, frame.getTitle(), "생성된 비밀번호가 클립보드에서 제거되었습니다. 만약 비밀번호가 유실되었을 경우 해당 홈페이지 비밀번호 찾기 기능을 이용하시길 바랍니다. ");;
+                    showAlert(mainView, mainView.getTitle(), "생성된 비밀번호가 클립보드에서 제거되었습니다. 만약 비밀번호가 유실되었을 경우 해당 홈페이지 비밀번호 찾기 기능을 이용하시길 바랍니다. ");;
                 }
             }
         });
@@ -343,7 +398,7 @@ public class PasswdGen extends WindowAdapter {
                     }
                 } catch(IOException | URISyntaxException exception) {
                     formatedLogcat("ERR", "페이지를 여는 중 예외 발생: " + exception.toString() + "\n\t수동으로 개발자 홈페이지에 방문해 주세요. " + url);
-                    showAlert(frame, frame.getTitle() + "- 오류", "이 OS는 현재 지원하지 않습니다. 로그캣에 작성된 URL을 복사하여 수동으로 여시기 바랍니다. ");;
+                    showAlert(mainView, mainView.getTitle() + "- 오류", "이 OS는 현재 지원하지 않습니다. 로그캣에 작성된 URL을 복사하여 수동으로 여시기 바랍니다. ");;
                 }
             }
         });
@@ -360,6 +415,7 @@ public class PasswdGen extends WindowAdapter {
                             if(exceptSimilarSymbol && generalSymbol) {
                                 formatedLogcat("INFO", "도움말" +
                                             "\n\t" + "help - 이 도움말 표시" +
+                                            "\n\t" + "count - 생성할 갯수 (현재 " + createPassword + "개)" +
                                             "\n\t" + "exceptsym {on/off} - 비슷한 문자 제외 켜짐" +
                                             "\n\t" + "unisym {on @#!$_-/off} - 특수문자 간략화 켜짐(" + exceptUniqueSymbol + ")" +
                                             "\n\t" + "clear - 버퍼 청소" +
@@ -368,6 +424,7 @@ public class PasswdGen extends WindowAdapter {
                             } else if(!exceptSimilarSymbol && generalSymbol) {
                                 formatedLogcat("INFO", "도움말" +
                                             "\n\t" + "help - 이 도움말 표시" +
+                                            "\n\t" + "count - 생성할 갯수 (현재 " + createPassword + "개)" +
                                             "\n\t" + "exceptsym {on/off} - 비슷한 문자 제외 꺼짐" +
                                             "\n\t" + "unisym {on @#!$_-/off} - 특수문자 간략화 켜짐(" + exceptUniqueSymbol + ")" +
                                             "\n\t" + "clear - 버퍼 청소" +
@@ -376,6 +433,7 @@ public class PasswdGen extends WindowAdapter {
                             } else if(exceptSimilarSymbol && !generalSymbol) {
                                 formatedLogcat("INFO", "도움말" +
                                             "\n\t" + "help - 이 도움말 표시" +
+                                            "\n\t" + "count - 생성할 갯수 (현재 " + createPassword + "개)" +
                                             "\n\t" + "exceptsym {on/off} - 비슷한 문자 제외 켜짐" +
                                             "\n\t" + "unisym {on @#!$_-/off} - 특수문자 간략화 꺼짐" +
                                             "\n\t" + "clear - 버퍼 청소" +
@@ -384,11 +442,25 @@ public class PasswdGen extends WindowAdapter {
                             } else {
                                 formatedLogcat("INFO", "도움말" +
                                             "\n\t" + "help - 이 도움말 표시" +
+                                            "\n\t" + "count - 생성할 갯수 (현재 " + createPassword + "개)" +
                                             "\n\t" + "exceptsym {on/off} - 비슷한 문자 제외 꺼짐" +
                                             "\n\t" + "unisym {on @#!$_-/off} - 특수문자 간략화 꺼짐" +
                                             "\n\t" + "clear - 버퍼 청소" +
                                             "\n\t" + "exit - 이 프로그램 종료"
                                 );
+                            }
+                            break;
+                        case "count":
+                            try {
+                                int input = Integer.parseInt(parameter[1].trim());
+                                if(input > 0 && input < 17) {
+                                    createPassword = input;
+                                    formatedLogcat("INFO", "비밀번호 생성 수가 성공적으로 변경됨");
+                                } else {
+                                    throw new NumberFormatException("For input number: " + input);
+                                }
+                            } catch (NumberFormatException e) {
+                                formatedLogcat("ERR", "비밀번호 생성 수를 설정하는 중 예외 발생: " + e.toString() + "\n\t" + parameter[1].trim() + " (은)는 비밀번호 생성 수를 설정할 수 있는 올바른 숫자가 아닙니다. ");
                             }
                             break;
                         case "exceptsym":
@@ -471,22 +543,27 @@ public class PasswdGen extends WindowAdapter {
 
                                 if(convert.length() == 0) {
                                     generalSymbol = false;
+                                    containUniqueSymbol.setLabel("특수문자 포함");
                                     throw new NullPointerException("두번째 파라미터에는 특수문자만 제공해야 합니다. ");
                                 } else if (isEdited && !flag) {
                                     formatedLogcat("INFO", "특수문자 선별기능이 활성화 되었지만 적합하지 않은 문자를 제외하고 다음 문자만 적용됨: " + exceptUniqueSymbol);
                                 } else if (isEdited && flag) {
-                                     formatedLogcat("INFO", "특수문자 선별기능이 활성화 되었지만 적합하지 않은 문자를 제외하고도 다음 문자가 무시될 수 있음: " + exceptUniqueSymbol);
+                                    formatedLogcat("INFO", "특수문자 선별기능이 활성화 되었지만 적합하지 않은 문자를 제외하고도 다음 문자가 무시될 수 있음: " + exceptUniqueSymbol);
                                 } else if (!isEdited && flag) {
                                     formatedLogcat("INFO", "특수문자 선별기능이 활성화 되었지만 다음 문자가 무시될 수 있음: " + exceptUniqueSymbol);
                                 } else {
                                     formatedLogcat("INFO", "특수문자 선별기능이 활성화 되었으며 다음 문자만 표시됨: " + exceptUniqueSymbol);
                                 }
                                 generalSymbol = true;
+                                containUniqueSymbol.setLabel("일부 특수문자 포함");
                             } else if(parameter[1].equals("on") && parameter[2].equals("")) {
+                                generalSymbol = false;
+                                containUniqueSymbol.setLabel("특수문자 포함");
                                 throw new NullPointerException("The parameter options are ambiguous.");
                             } else {
                                 generalSymbol = false;
                                 exceptUniqueSymbol = "";
+                                containUniqueSymbol.setLabel("특수문자 포함");
                                 formatedLogcat("INFO", "특수문자 선별기능이 비활성화 되었음");
                             }
                             break;
